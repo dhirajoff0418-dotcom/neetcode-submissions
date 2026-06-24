3c15f06369df425e1970e549358981b846bd34d9
dhirajoff0418-dotcom <dhirajoff0418@gmail.com>
2026-06-24 11:51:08 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MissingNumber(int[] nums) {
+       Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != i)
+            {
+                return i;
+            }
+        }
+
+        return nums.Length;
+    }
+}
